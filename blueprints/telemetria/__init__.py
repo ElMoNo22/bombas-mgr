7d63914b15192ecef3adf6420902796749dffddd
ElMoNo22 <138520380+ElMoNo22@users.noreply.github.com>
2026-06-10 09:25:49 -0300

    Add files via upload

--- blueprints/telemetria/__init__.py
+++ blueprints/telemetria/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-tele_bp = Blueprint('telemetria', __name__, url_prefix='/telemetria',
-                    template_folder='../../templates/telemetria')
+tele_bp = Blueprint('telemetria', __name__, url_prefix='/telemetria')
 
-from . import routes  # noqa
+from . import routes
